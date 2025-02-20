a189a564652f3da6fd9fb17d3c947c81f8b63868
NITHIN CS <157794860+NITHINCS2004@users.noreply.github.com>
2025-02-20 19:52:20 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,1 @@
-module.exports = {
-    content: [
-      './src/**/*.{js,jsx,ts,tsx}', // Tailwind should look through all your JS and TS files
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
+
