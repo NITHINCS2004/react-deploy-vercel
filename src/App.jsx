61bfcb800200de9781dbe1c3bd45fbb3d3c443cb
123456NI-GIT <157794860+123456NI-GIT@users.noreply.github.com>
2025-02-23 15:21:28 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
 function App() {
   return (
-    /*<Router>
+    <Router>
       <Routes>
         <Route path="/" element={<PhoneSignin />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </Router>*/
-    <div>hello</div>
+    </Router>
+    
   );
 }
 
